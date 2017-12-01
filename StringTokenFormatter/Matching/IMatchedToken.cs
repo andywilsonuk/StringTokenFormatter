72a9cdc18c7ec52bc0f375ccecfdc44ed8b85c73
andy <code@andywilson.me.uk>
2017-12-01 15:55:37 +0000

    Removed unused usings

--- StringTokenFormatter/Matching/IMatchedToken.cs
+++ StringTokenFormatter/Matching/IMatchedToken.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace StringTokenFormatter
+﻿namespace StringTokenFormatter
 {
     public interface IMatchedToken
     {
--- StringTokenFormatter/TokenReplacer.cs
+++ StringTokenFormatter/TokenReplacer.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace StringTokenFormatter
 {
