3d5fdbe82051bc922d54f8e915137d85e8ef2bf8
Andy Wilson <wilsona@rsc.org>
2019-04-01 10:44:34 +0100

    Split ObjectPropertiesTokenValueContainer. Bumper version to 3.1

---
+++ StringTokenFormatter/Containers/NonLockingLazy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StringTokenFormatter
+{
+    /// <summary>
+    /// This class mimics the System.Lazy type except it specifically does not have locking implemented
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class NonLockingLazy<T>
+    {
+        private readonly Func<T> creator;
+
+        public NonLockingLazy(Func<T> creator)
+        {
+            this.creator = creator;
+        }
+
+        public bool IsValueCreated { get; private set; }
+        public T CreatedValue { get; private set; }
+
+        public T Value
+        {
+            get
+            {
+                if (!IsValueCreated)
+                {
+                    CreatedValue = creator();
+                    IsValueCreated = true;
+                }
+                return CreatedValue;
+            }
+        }
+    }
+}
--- StringTokenFormatter/Containers/ObjectPropertiesTokenValueContainerGeneric.cs
+++ StringTokenFormatter/Containers/ObjectPropertiesTokenValueContainerGeneric.cs
@@ -2,30 +2,33 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Threading;
+using System.Text;
 
 namespace StringTokenFormatter
 {
     /// <summary>
-    /// Converts all of an objects properties to a Token Value Converter.
+    /// Converts only the properties exposed by {T} (but not any members on derived classes) to a token value container.
+    /// This implementation runs ~15% faster than the non-generic version by caching the TypeDescriptor lookups.
     /// </summary>
-    public class ObjectPropertiesTokenValueContainer : ITokenValueContainer
+    /// <typeparam name="T">A type indicating the exact properties that will be used for formatting.</typeparam>
+    public class ObjectPropertiesTokenValueContainer<T> : ITokenValueContainer
     {
+        private static readonly List<PropertyDescriptor> propertyDescriptors = TypeDescriptor.GetProperties(typeof(T)).OfType<PropertyDescriptor>().ToList();
         private IDictionary<string, NonLockingLazy<object>> dictionary;
         private readonly ITokenMatcher matcher;
 
-        public ObjectPropertiesTokenValueContainer(object tokenValueObject, ITokenMatcher tokenMatcher)
+        public ObjectPropertiesTokenValueContainer(T tokenValueObject, ITokenMatcher tokenMatcher)
         {
             if (tokenValueObject == null) throw new ArgumentNullException(nameof(tokenValueObject));
             matcher = tokenMatcher ?? throw new ArgumentNullException(nameof(tokenMatcher));
             dictionary = ConvertObjectToDictionary(tokenValueObject);
         }
 
-        private IDictionary<string, NonLockingLazy<object>> ConvertObjectToDictionary(object values)
+        private IDictionary<string, NonLockingLazy<object>> ConvertObjectToDictionary(T values)
         {
             var mappings = new Dictionary<string, NonLockingLazy<object>>(matcher.TokenNameComparer);
 
-            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(values))
+            foreach (var descriptor in propertyDescriptors)
             {
                 mappings[descriptor.Name] = new NonLockingLazy<object>(() => descriptor.GetValue(values));
             }
