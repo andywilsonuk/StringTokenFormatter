4ae7642db96900ec214c54522a715d3784514da5
andywilsonuk <code@andywilson.me.uk>
2024-01-21 15:58:21 +0000

    Parity tests

--- StringTokenFormatter.Tests/ScenarioTests.cs
+++ StringTokenFormatter.Tests/ScenarioTests.cs
@@ -8,11 +8,23 @@ public class ScenarioTests
     [Fact]
     public void LoopAndConditional_StringWithLiteralValue3TimesWithoutSuppressedValue()
     {
-        string source = @"{:loop:3}lit{:if,IsValid}suppressed{:ifend}{:loopend}";
+        string source = "{:loop:3}lit{:if,IsValid}suppressed{:ifend}{:loopend}";
         valuesContainer.Add("IsValid", false);
 
         string actual = resolver.FromContainer(source, valuesContainer);
 
         Assert.Equal("litlitlit", actual);
+    }
+
+    [Fact]
+    public void ConditionalPreventsInnerTokenMatching_StringWithoutSuppressedValue()
+    {
+        string source = "{:if,IsValid}{Suppressed}{:ifend}";
+        valuesContainer.Add("IsValid", false);
+        valuesContainer.Add("Suppressed", () => Assert.Fail("This value should not be called"));
+
+        string actual = resolver.FromContainer(source, valuesContainer);
+
+        Assert.Equal(string.Empty, actual);
     }
 }
