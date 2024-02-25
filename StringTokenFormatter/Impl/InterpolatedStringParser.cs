9752d96e9f1d9ee947f80b098479fb8cd7eeba60
andywilsonuk <code@andywilson.me.uk>
2024-02-25 11:17:53 +0000

    Parser patter matching on component parts

--- StringTokenFormatter/Impl/InterpolatedStringParser.cs
+++ StringTokenFormatter/Impl/InterpolatedStringParser.cs
@@ -5,10 +5,10 @@ namespace StringTokenFormatter.Impl;
 public static partial class InterpolatedStringParser
 {
     private const string commandPrefix = ":";
-    private const string pseudoPrefix = commandPrefix + commandPrefix;
+    private const string pseudoPrefix = "::";
     private const string paddingSeparator = ",";
     private const string formattingSeparator = ":";
-    private const string tokenComponentsPattern = $"^({commandPrefix}{{0,2}})?([^{paddingSeparator}{formattingSeparator}]*){paddingSeparator}?([^{formattingSeparator}]*){formattingSeparator}?(.*)$";
+    private const string tokenComponentsPattern = $"^({pseudoPrefix}|{commandPrefix})?([^{paddingSeparator}{formattingSeparator}]*){paddingSeparator}?([^{formattingSeparator}]*){formattingSeparator}?(.*)$";
     private static readonly RegexOptions regexOptions = RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant;
 
 #if NET8_0_OR_GREATER
@@ -83,20 +83,13 @@ public static partial class InterpolatedStringParser
     private static InterpolatedStringSegment ConvertFromComponents(string componentPartsString, string segment)
     {
         string[] components = GetTokenComponentsRegex().Split(componentPartsString);
-        string commandComponent = components[1];
-        string nameComponent = components[2];
-        bool isCommand = OrdinalValueHelper.AreEqual(commandPrefix, commandComponent);
-        if (isCommand)
+        InterpolatedStringTokenOnlySegment parsedSegment = components switch
         {
-            if (nameComponent == string.Empty) { throw new ParserException($"Blank command name in matched: {segment}"); }
-            return new InterpolatedStringCommandSegment(segment, nameComponent, components[3], components[4]);
-        }
-        if (nameComponent == string.Empty) { throw new ParserException($"Blank token name in matched: {segment}"); }
-        bool isPseudo = OrdinalValueHelper.AreEqual(pseudoPrefix, commandComponent);
-        if (isPseudo)
-        {
-            return new InterpolatedStringPseudoTokenSegment(segment, commandComponent + nameComponent, components[3], components[4]);
-        }
-        return new InterpolatedStringTokenSegment(segment, nameComponent, components[3], components[4]);
+            [_, commandPrefix, string commandName, string tokenName, string data, _] => new InterpolatedStringCommandSegment(segment, commandName, tokenName, data),
+            [_, pseudoPrefix, string tokenName, string alignment, string format, _] when tokenName != string.Empty => new InterpolatedStringPseudoTokenSegment(segment, pseudoPrefix + tokenName, alignment, format),
+            [_, string tokenName, string alignment, string format, _] when tokenName != string.Empty => new InterpolatedStringTokenSegment(segment, tokenName, alignment, format),
+            _ => throw new ParserException($"Unable to parse segment: {segment}"),
+        };
+        return parsedSegment;
     }
 }
